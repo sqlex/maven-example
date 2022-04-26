fac06c050bdb43195e1cd658ce111b668052929a
李雨田 <demon@danwi.me>
2022-04-26 12:46:52 +0800

    更新项目,适配最近的0.3.0版本

--- src/main/java/me/danwi/sqlex/example/Main.java
+++ src/main/java/me/danwi/sqlex/example/Main.java
@@ -1,25 +1,27 @@
 package me.danwi.sqlex.example;
 
-import me.danwi.sqlex.core.DataSource;
-import me.danwi.sqlex.core.DataSourceManager;
+import me.danwi.sqlex.core.DaoFactory;
 import me.danwi.sqlex.example.dao.RoleDao;
 import me.danwi.sqlex.example.dao.UserDao;
 
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
-        final DataSourceManager manager = new DataSourceManager();
-        final DataSource<Repository> dataSource = manager.getInstance(Repository.class);
+    public static void main(String[] args) throws Exception {
+        DaoFactory factory = new DaoFactory(
+                "jdbc:mysql://localhost:3306/sqlex",
+                "root", "1234qwer",
+                Repository.class
+        );
 
-        UserDao userDao = dataSource.getInstance(UserDao.class);
-        RoleDao roleDao = dataSource.getInstance(RoleDao.class);
+        UserDao userDao = factory.getInstance(UserDao.class);
+        RoleDao roleDao = factory.getInstance(RoleDao.class);
 
-        userDao.getAll(10, "123", null);
-        userDao.getAll(10, "123", new ArrayList());
-        userDao.getAllByRole("123");
-        userDao.getCountsByRole();
+        System.out.println(userDao.getAll(10, "123", null));
+        System.out.println(userDao.getAll(10, "123", new ArrayList()));
+        System.out.println(userDao.getAllByRole("123"));
+        System.out.println(userDao.getCountsByRole());
 
-        roleDao.getAll("管");
+        System.out.println(roleDao.getAll("管"));
     }
 }
